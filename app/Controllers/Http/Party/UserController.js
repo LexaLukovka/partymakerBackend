fa3d3393fb13244568efd088ce0b6998a1689412
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2018-08-12 20:36:26 +0300

    update party members

--- app/Controllers/Http/Party/UserController.js
+++ app/Controllers/Http/Party/UserController.js
@@ -8,26 +8,36 @@ class UserController {
   // noinspection JSUnusedGlobalSymbols
   async index({ params }) {
     const party = await Party.find(params.party_id)
-    return party.users().fetch()
+
+    return {
+      admin: await party.admin().first(),
+      data: await party.users().orderBy('id', 'desc').fetch()
+    }
   }
 
   async store({ params, auth, response }) {
     const party = await Party.find(params.party_id)
 
-    if (await party.isOnParty(auth.user.id)) {
-      return response.status(403).json({
-        status: 403,
-        success: false,
-        message: `${auth.user.name} already joined party`
-      })
-    }
-
     await party.users().attach([auth.user.id])
 
     return {
-      status: 200,
-      success: true,
+      party_id: party.id,
       message: `${auth.user.name} joined the party`
+    }
+  }
+
+  async show({ params, auth }) {
+    const party = await Party.find(params.party_id)
+    const users = await party.users().fetch()
+
+    if (params.id === 'isMember') {
+      return {
+        isMember: await party.isOnParty(auth.user.id)
+      }
+    }
+
+    return {
+      isMember: await party.isOnParty(params.id)
     }
   }
 
@@ -35,19 +45,9 @@ class UserController {
 
     const party = await Party.find(params.party_id)
 
-    if (parseInt(params.id) !== auth.user.id) {
-      return response.status(403).json({
-        status: 403,
-        success: false,
-        message: 'you cannot remove other users from party'
-      })
-    }
-
     if (!await party.isOnParty(auth.user.id)) {
 
       return response.status(403).json({
-        status: 403,
-        success: false,
         message: `${auth.user.name} was not on party`
       })
     }
@@ -55,8 +55,7 @@ class UserController {
     await party.users().detach([auth.user.id])
 
     return {
-      status: 200,
-      success: true,
+      party_id: party.id,
       message: `${auth.user.name} left the party`
     }
   }
