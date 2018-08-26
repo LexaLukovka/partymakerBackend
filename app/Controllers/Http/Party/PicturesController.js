6669c7e2d2ab52bed6b68ed7b0ef9b5e8c0795d2
luker <lukerlexa@gmail.com>
2018-08-26 18:05:27 +0300

    add remove file (pictures) in upload

--- app/Controllers/Http/Party/PicturesController.js
+++ app/Controllers/Http/Party/PicturesController.js
@@ -1,9 +1,18 @@
+/* eslint-disable global-require */
 const Env = use('Env')
+const Helpers = use('Helpers')
+
 const Pictures = use('App/Models/Picture')
 
 class PicturesController {
   // noinspection JSUnusedGlobalSymbols
-  async destroy({ params }) {
+  async destroy({ request, params }) {
+    const fs = require('fs')
+    const removeFile = Helpers.promisify(fs.unlink)
+    const location = Helpers.publicPath('uploads')
+
+    removeFile(`${location}\\${params.picture}`)
+
     await Pictures
       .query()
       .where({ url: `${Env.get('APP_URL')}/uploads/${params.picture}` })
