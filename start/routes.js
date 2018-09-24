927923046852cb2f21d2e9f5f45f3d8daa4cf015
pavliha <kostyk.pavel.09@gmail.com>
2018-09-24 20:12:21 +0300

    fix routes

--- start/routes.js
+++ start/routes.js
@@ -3,15 +3,7 @@ const Route = use('Route')
 
 const SUD = new Map([[['store', 'update', 'destroy'], ['auth']]])
 
-const resource = (path, controller, middleware) => {
-  const name = controller.replace('Controller', '')
-  return Route.resource(path, controller)
-    .validator([[`${path}.store`, `${name}/Create`], [`${path}.update`, `${name}/Edit`]])
-    .middleware(middleware)
-}
-
-resource('/', 'DocsController')
-
+Route.get('/', 'DocsController.index')
 /**
  *
  * Authentication routes
@@ -29,7 +21,9 @@ Route.get('user', 'AuthController.user').middleware('auth')
  *
  * */
 
-resource('users', 'UserController', SUD)
+Route.resource('users', 'UserController')
+  .validator([['users.store', 'User/Create'], ['users.update', 'User/Edit']])
+  .middleware(SUD)
 
 /**
  *
@@ -37,23 +31,31 @@ resource('users', 'UserController', SUD)
  *
  * */
 
-resource('places', 'PlaceController', SUD)
+Route.resource('places', 'PlaceController')
+  .validator([['places.store', 'Place/Create'], ['places.update', 'Place/Edit']])
+  .middleware(SUD)
 
 /**
  *
  * Group routes
  *`
  * */
+Route.resource('groups/:group_id/members', 'Group/MemberController')
+  .validator([['groups/:group_id/members.store', 'Group/Member/Create']])
+  .middleware('auth')
 
-resource('groups/:group_id/members', 'Group/MemberController', 'auth')
-resource('groups', 'Group/MemberController', SUD)
+Route.resource('groups', 'GroupController')
+  .validator([['groups.store', 'Group/Create'], ['groups.update', 'Group/Edit']])
+  .middleware(SUD)
 
 /**
  *
  * Events routes
  *`
  * */
-resource('events', 'EventController', SUD)
+Route.resource('events', 'EventController')
+  .validator([['events.store', 'Event/Create']])
+  .middleware(SUD)
 
 /**
  *
@@ -61,12 +63,7 @@ resource('events', 'EventController', SUD)
  *
  * */
 
-resource('places/:place_id/votes', 'Place/RatingController', SUD)
-resource('places', 'PlaceController', SUD)
+Route.resource('places/:place_id/votes', 'Place/RatingController').middleware('auth')
+Route.resource('places', 'Place/PlaceController').middleware(SUD)
 
-/**
- *
- * Upload routes
- *
- * */
-resource('upload', 'UploadController', SUD)
+Route.resource('upload', 'UploadController')
