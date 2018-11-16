9a051ed43e61a790b62566e5e946826a7bd29fa8
LexaLukovka <lukerlexa@gmail.com>
2018-11-16 12:46:29 +0200

    add myself in seeds)

--- database/seeds/users.js
+++ database/seeds/users.js
@@ -6,4 +6,11 @@ module.exports = [
     password: 'qwerty123',
     avatar_url: '/images/dummy/pavliha.jpg',
   },
+  {
+    name: 'Aleksey Lukovka',
+    email: 'lukerlexa@gmail.com',
+    phone: '+380990848975',
+    password: '123123',
+    avatar_url: '/images/dummy/aleksey.jpeg',
+  },
 ]
